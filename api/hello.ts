f97e7fda535e648fcda0cf220926788b47bbb8c4
abhishek <abhishekchorotiya@openinapp.com>
2024-07-14 05:28:24 +0530

    update

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,35 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
-  return res.json({
-    message: `Hello ${name}!`,
-  })
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import axios from "axios";
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  if (req.method === "GET" && req.url === "/hello") {
+    return res.json({ message: "Hello World, from Vercel!" });
+  } else if (req.method === "POST" && req.url === "/api/compile") {
+    try {
+      const { language, fileName, code } = req.body;
+      const headersList = {
+        Accept: "*/*",
+        "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+        "Content-Type": "application/json",
+      };
+      const bodyContent = JSON.stringify({
+        properties: {
+          language,
+          files: [{ name: fileName, content: code }],
+          stdin: null,
+        },
+      });
+      const reqOptions = {
+        url: "https://onecompiler.com/api/code/exec",
+        method: "POST",
+        headers: headersList,
+        data: bodyContent,
+      };
+      const response = await axios.request(reqOptions);
+      return res.json(response.data);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  } else {
+    return res.status(404).json({ message: "Not Found" });
+  }
 }
